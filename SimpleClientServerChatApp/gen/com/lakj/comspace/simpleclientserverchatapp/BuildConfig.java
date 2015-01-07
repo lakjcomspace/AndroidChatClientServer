487fb95739c26545f2e27adef59641c06eaa50c4
lakjcomspace <lakjcomspace@gmail.com>
2015-01-07 21:10:38 +0530

    Android client-server chat app.

---
+++ SimpleClientServerChatApp/gen/com/lakj/comspace/simpleclientserverchatapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.lakj.comspace.simpleclientserverchatapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
